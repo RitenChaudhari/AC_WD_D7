151b37736d1ca077969be63ba1d92c2785875e33
RitenChaudhari <ritenchaudhari@gmail.com>
2025-10-26 13:13:37 +0530

    Learning Basic Javascript

---
+++ 11-JavaScript(Part-II)/1-Basic/Assignment/assignJs.js
@@ -0,0 +1,72 @@
+// console.log("----------Q1-----------");
+
+// let num = 10;
+
+// if(num % 10 == 0){
+//     console.log("good");
+// }else{
+//     console.log("bad");
+// }
+
+// console.log("---------------Q2-----------------");
+
+// let name = prompt("Enter Name: ");
+// let age = prompt("Enter Age: ");
+
+// alert(`${name} is ${age} years old.`);
+
+// console.log("----------------Q3---------------");
+
+// let month = 1;
+
+// switch(month){
+//     case 1:
+//         console.log("January,February,March");
+//         break;
+//     case 2:
+//         console.log("April,May,June");
+//     case 3:
+//         console.log("July,August,September");
+//     case 4:
+//         console.log("October,November,December");
+// }
+
+console.log("--------------Q4---------------");
+
+let str = 'Apna College';
+
+if((str[0] = 'A' ||str[0] == 'a') && str.length > 5){
+    console.log("Golden String");
+}else{
+    console.log("Not a golden string");
+}
+
+let a = 1112;
+let b = 1212;
+let c = 311;
+
+if(a>b){
+    if(a>c){
+        console.log("a is greatest");
+    }else{
+        console.log("c is greatest");
+    }
+}else{
+    if(b>c){
+        console.log("b is greatest");
+    }else{
+        console.log("c is greatest");
+    }
+}
+
+
+console.log("-----------------Q6----------------");
+
+let num1 = 321;
+let num2 = 47852;
+
+if(num1 % 10 == num2 % 10){
+    console.log("Same last digit");
+}else{
+    console.log("Nein");
+}
---
+++ 11-JavaScript(Part-II)/1-Basic/app.js
@@ -0,0 +1,96 @@
+// console.log("Hello World!");
+// let a = 15;
+// let b = 20;
+
+// console.log(`Sum :${a+b} is the answer`);
+
+// console.log(16>18);
+// console.log(19>18);
+
+// let color  = 'Yellow';
+
+// if(color == 'Red'){
+//     console.log("Stop");
+// }
+
+// else if(color == 'Yellow'){
+//     console.log("Slow Down");
+// }
+
+// else{
+//     console.log("Go");
+// }
+
+// let size = "XL";
+
+// if(size === "XL"){
+//     console.log("Rs 250");
+// }
+
+// if(size === "L"){
+//     console.log("Rs 200");
+// }
+
+// if(size === "M"){
+//     console.log("Rs 100");
+// }
+
+// if(size === "S"){
+//     console.log("Rs 50");
+// }
+
+// console.log("---------------------------");
+
+// let str = "apna";
+
+// if(str.length > 3 && str[0] == 'a'){
+//     console.log("Good String");
+// }else{
+//     console.log("Not a good String");
+// }
+
+// console.log("---------------------------");
+
+// let day = 4;
+
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+    
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+    
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+    
+//     case 4:
+//         console.log("Thursday");
+//         break;
+    
+//     case 5:
+//         console.log("Friday");
+//         break;
+    
+//     case 6:
+//         console.log("Saturday");
+//         break;
+    
+//     case 7:
+//         console.log("Sunday");
+//         break;
+
+//     default:
+//         console.log("Enter Valid Number");
+// }
+
+// console.error("Error hai bhai!");
+// console.warn("Warning hai bhai!");
+
+let firstName = prompt("Enter First Name: ");
+let lastName = prompt("Enter Last Name: ");
+let msg = `Name is ${firstName +" "+ lastName}`;
+
+console.log(msg);
