97051dc6c2fff6cf6ccd1abed12b900b8cb2f3f0
RitenChaudhari <ritenchaudhari@gmail.com>
2025-10-29 22:14:49 +0530

    JavaScript Fundamentals

---
+++ 14-JavaScript(Part-V)/Assignment/assign5.js
@@ -0,0 +1,20 @@
+//Q1
+console.log("-----------Q1-------------");
+console.log(Math.floor(Math.random() * 6)+1);
+
+//Q2
+console.log("-----------Q2-------------");
+const Car = {
+    name:"Honda Elevate",
+    model:"DDIS",
+    color:"White",
+}
+
+//Q3
+console.log("-----------Q3-------------");
+
+const Person = {
+    name:"Riten",
+    Age: 24,
+    City:"Ahmedabad",
+}
---
+++ 14-JavaScript(Part-V)/script.js
@@ -0,0 +1,21 @@
+const post = {
+    username:"@RitenChaudhari",
+    content:"This is My First Post",
+    likes:150,
+    reposts:5,
+    tags:["@apnacollege","delta"]
+}
+
+//Generate a random number between 1 and 100.
+
+let rand = Math.random();
+rand = (rand * 100) + 1;
+rand = Math.floor(rand);
+console.log(rand); 
+
+//Generate a random number between 1 and 5.
+
+let rando = Math.random();
+rando = (rando*5)+1;
+rando = Math.floor(rando);
+console.log(rando);
